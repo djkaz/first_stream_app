859806e3e6be0b5c7579c3962ff90bb4ca091396
Tiron Alexandru-Stefan <stefiaxe@yahoo.com>
2022-10-13 10:03:05 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-##streamlit.dataframe(my_fruit_list)
 
 #Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
@@ -42,7 +41,13 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 #snowflake connector
 import snowflake.connector
 
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 #my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 #my_data_row = my_cur.fetchone()
@@ -54,34 +59,5 @@ import snowflake.connector
 #streamlit.text(my_data_row)
 
 
-###test streamlit
-
-import snowflake.connector
- 
-print("Connecting...")
-
-con = snowflake.connector.connect(
- user="alextiron",
- password="Ax#l198901",
- account="ie64300.eu-west-1.aws",
- warehouse="COMPUTE_WH",
- database="SNOWFLAKE_SAMPLE_DATA",
- schema="PUBLIC"
-
-)
-print(con)
-con.cursor().execute("USE WAREHOUSE " + WAREHOUSE)
-con.cursor().execute("USE DATABASE " + DATABASE)
-
-try:
-
-  result = con.cursor().execute("Select * from REGION")
-  result_list = result.fetchall()
-  print(result_list)
-
-finally:
-
-con.cursor().close()
-con.cursor().close()
 
 
